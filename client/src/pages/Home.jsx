72ef6d5577c12df3dc9c76eeab13157105eb85b3
Kushal Karan <85029373+codepley@users.noreply.github.com>
2024-01-14 13:13:38 +0530

    Update Home.jsx

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -61,7 +61,7 @@ const Home = () => {
     <section className="max-w-7xl mx-auto">
       <div>
         <h1 className="font-extrabold text-[#222328] text-[32px]">
-          Community Showcase
+          Imagen - Community Showcase
         </h1>
         <p className="mt-2 text-[#666e75] text-[16px]">
           Browse through a collection of imaginative and visually stunning
